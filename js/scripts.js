5c7957d07d672c2fe2c3cb53f9a7463050d89c0d
Aaron Nguyen and LaTanya Donaldson <latanya@>
2017-03-23 16:32:46 -0700

    initial commit, basic framework for tracker

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var ab = parseInt($("input#ab").val());
+    var bc = parseInt($("input#bc").val());
+    var ac = parseInt($("input#ac").val());
+
+    {
+    if (ab)
+      if (bc && ac)
+
+    if (ab === bc === ac) {
+      alert("you have an equilateral");
+    }
+
+  };
+  });
+  });
